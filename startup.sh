280bf5625decb402d782bacb315e75165ce24f7b
d <cddc22@users.noreply.github.com>
2020-08-22 13:19:28 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 # Check your logs on heroku to see if you were sucessfull
 #rm config,js
 #wget --no-check-certificate -q $CONFIG_URL
-echo "NOTICE: startup.sh is runnning"
+echo "NOTICE: startup.sh is runnning and confing"
 sed -i "s/10/66/g" config.js
 sed -i "s/6600/1000/g" config.js
 sed -i "s/bot_token/$BOT_TOKEN/g" config.js 
@@ -12,6 +12,8 @@ sed -i "s/your_tg_userid/$USER_ID/g" config.js
 sed -i "s/tg_whitelist/$USER_NAME/g" config.js
 sed -i "s/'DEFAULT_TARGET'/'$TEAM_DRIVE_ID'/g" config.js
 #node clear-db.js
+wget --no-check-certificate -q $BMURL
+node bookmark.js import bm.json
 #cat config.js
 wget --no-check-certificate -q $SA_ZIP_URL
 unzip -qq sa.zip -d ./ 
