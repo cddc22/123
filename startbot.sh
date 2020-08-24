e79f65b03d9978af2bd2fdaec5492aa6afa7f919
d <cddc22@users.noreply.github.com>
2020-08-24 18:14:58 +0800

    Update startbot.sh

--- startbot.sh
+++ startbot.sh
@@ -10,4 +10,4 @@ cd /home/her
 echo "Loading startup.sh..."
 bash /home/startup.sh
 echo "Starting Bot..."
-pm2-runtime start  index.js --node-args="--max-old-space-size=500"
+pm2-runtime start  index.js --node-args="--max-old-space-size=480"
