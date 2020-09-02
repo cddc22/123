3977c725fdff0b2ba77d6e20d45fda61d7cf223e
d <cddc22@users.noreply.github.com>
2020-09-02 15:09:59 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -3,9 +3,9 @@
 # put your commands to install extra shit below:
 # Check your logs on heroku to see if you were sucessfull
 git clone https://github.com/dissipator/gd-utils.git her
-#git clone https://github.com/cddc22/her.git
+
 cd her
-#cd gd-utils
+
 npm install --unsafe-perm=true --allow
 npm config set unsafe-perm=true
 rm -rf node_module
@@ -14,8 +14,8 @@ npm i
 npm config set python
 npm i pm2 -g
 
-sed -i "s/10/255/g" config.js
-sed -i "s/25500/1000/g" config.js
+sed -i "s/10/888/g" config.js
+sed -i "s/88800/1000/g" config.js
 
 #ls
 cat config.js
