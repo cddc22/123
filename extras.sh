3d72f08d49da1cd2d871041064e220be0f8785ec
d <cddc22@users.noreply.github.com>
2020-09-02 15:07:19 +0800

    Update extras.sh

--- extras.sh
+++ extras.sh
@@ -13,13 +13,12 @@ npm i
 #npm install -g node-gyp 
 npm config set python
 npm i pm2 -g
-#cd /home/gd-utils
-#sed -i "s/bot_token/$BOT_TOKEN/g" .config.js &&
-#sed -i "s/your_tg_userid/$USER_ID/g" .config.js 
-#sed -i "s/tg_whitelist/$USER_NAME/g" .config.js
-#sed -i "s/DEFAULT_TARGET = ''/DEFAULT_TARGET = '$TEAM_DRIVE_ID'/g" .config.js
+
+sed -i "s/10/255/g" config.js
+sed -i "s/25500/1000/g" config.js
+
 #ls
-#cat config.js
+cat config.js
 
 
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
